06845979f417cb750b793dc06018a16eac1dd756
chris <chris.janssom@gmail.com>
2018-10-30 16:57:22 +0100

    Add sample giraffe pipeline

--- Farss.Server/Startup.fs
+++ Farss.Server/Startup.fs
@@ -5,17 +5,23 @@ open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.DependencyInjection
+open Giraffe
+
 
 type Startup() =
+    let webApp =
+        choose [
+            route "/ping"   >=> text "pong"
+            route "/"       >=> htmlFile "/pages/index.html" ]
 
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     member this.ConfigureServices(services: IServiceCollection) =
-        ()
+        services.AddGiraffe() |> ignore
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     member this.Configure(app: IApplicationBuilder, env: IHostingEnvironment) =
         if env.IsDevelopment() then 
             app.UseDeveloperExceptionPage() |> ignore
 
-        app.Run(fun context -> context.Response.WriteAsync("Hello World!")) |> ignore
+        app.UseGiraffe webApp
