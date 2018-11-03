fe137f4202691b9522c3ad3560e2da328043622c
chris <chris.janssom@gmail.com>
2018-11-03 09:55:40 +0100

    Wrap stuff

--- Farss.Tests/Specs/SubscribeToFeedSpecs.fs
+++ Farss.Tests/Specs/SubscribeToFeedSpecs.fs
@@ -3,6 +3,10 @@ module SubscribeToFeedSpecs
 open Expecto
 open Microsoft.AspNetCore.Mvc.Testing
 
+module HttpClient = 
+    let getAsync (url: string) (client: System.Net.Http.HttpClient) =
+        client.GetAsync(url) |> Async.AwaitTask
+
 [<Tests>]
 let tests = 
     testList "Subscribe to feed specs" [
@@ -19,8 +23,18 @@ let tests =
             let factory = new WebApplicationFactory<Farss.Server.Startup>()
             let client = factory.CreateClient()
 
-            let! response = client.GetAsync("/ping") |> Async.AwaitTask
+            let! response = client |> HttpClient.getAsync "/ping"
+
+            response.EnsureSuccessStatusCode() |> ignore
+        }
+        
+        testAsync "In memory server" {
+            let factory = new WebApplicationFactory<Farss.Server.Startup>()
+            let client = factory.CreateClient()
+
+            let! response = client |> HttpClient.getAsync "/ping"
 
             response.EnsureSuccessStatusCode() |> ignore
         }
     ]
+
