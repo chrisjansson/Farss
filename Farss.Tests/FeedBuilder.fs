62b0bcb6def5ba9396d96fd10aa87fcef8f48e57
chris <chris.janssom@gmail.com>
2018-11-07 19:58:17 +0100

    Build feeds in process

---
+++ Farss.Tests/FeedBuilder.fs
@@ -0,0 +1,26 @@
+module FeedBuilder 
+
+open System.Xml
+open Microsoft.SyndicationFeed
+open Microsoft.SyndicationFeed.Rss
+
+type StringWriterWithEncoding(encoding: System.Text.Encoding) =
+    inherit System.IO.StringWriter()
+
+    override this.Encoding with get() = encoding
+
+let feed (title: string) = SyndicationItem(Title = title)
+
+let withDescription (description: string) (item: SyndicationItem) =
+    item.Description <- description
+    item
+
+let toRss (feed: SyndicationItem) = 
+    let sw = new StringWriterWithEncoding(System.Text.Encoding.UTF8);
+    use xmlWriter = XmlWriter.Create(sw, XmlWriterSettings(Async=true))
+    let writer = RssFeedWriter(xmlWriter)
+    (writer.Write(feed)).Wait()
+    (writer.Flush()).Wait()
+    xmlWriter.Flush()
+    xmlWriter.Dispose()
+    sw.ToString()
