b838f97f105690614cb3ad808a1cbc36932c97a2
chris <chris.janssom@gmail.com>
2018-11-25 19:29:55 +0100

    FeedReaderStub

--- Farss.Tests/TestInfrastructure/FakeFeedReaderAdapter.fs
+++ Farss.Tests/TestInfrastructure/FakeFeedReaderAdapter.fs
@@ -9,8 +9,28 @@ let parseError (message: string) =
     Exception(message) |> ParseError  |> Error |> AsyncResult.Return
 
 let fetchError (message: string) =
-    Exception(message) |> FetchError  |> Error |> AsyncResult.Return
+    Exception(message) |> FetchError  |> Error |> AsyncResult.Return 
 
-let feed (feed: CodeHollow.FeedReader.Feed) =
-    feed |> Ok  |> AsyncResult.Return
+type FeedReaderAdapterStub = 
+    {
+        SetResult: string * Result<FeedReaderAdapter.Feed, FeedReaderAdapter.FeedError> -> unit
+        Adapter: FeedReaderAdapter.FeedReaderAdapter
+    }
 
+let createStub ():FeedReaderAdapterStub  =
+    let mutable results = Map.empty
+
+    let setResult (url, result) =
+        results <- Map.add url result results
+        
+    let getFromUrl url =
+        async.Return (Map.find url results)
+
+    let adapter: FeedReaderAdapter.FeedReaderAdapter = 
+        {
+            FeedReaderAdapter.FeedReaderAdapter.getFromUrl = getFromUrl
+        }
+    {
+        SetResult = setResult
+        Adapter = adapter
+    }
