8473b07a6b5d5f9bb735c84477d6e9bd34e00e8f
chris <chris.janssom@gmail.com>
2018-11-25 20:11:02 +0100

    Couple of fetch artiles workflow tests

--- Farss.Server/FetchEntriesWorkflow.fs
+++ Farss.Server/FetchEntriesWorkflow.fs
@@ -5,6 +5,7 @@ open Persistence
 open FeedReaderAdapter
 open FSharp.Control.Tasks.V2
 open System
+open System.Threading.Tasks
 
 
 let fetchEntries 
@@ -14,12 +15,31 @@ let fetchEntries
     _ = task {
         let subscriptions = subscriptionRepository.getAll()
     
-        for s in subscriptions do
-            let! getFeedResult = adapter.getFromUrl(s.Url)
-            match getFeedResult with
-            | Ok feed ->
-                for item in feed.Items do
-                    let article = { Title = item.Title; Id = Guid.NewGuid() }
-                    articleRepository.save(article)
-            | Error e -> ()
+        let tasks = 
+            subscriptions 
+            |> List.map (fun s -> adapter.getFromUrl s.Url)
+            |> List.map Async.StartAsTask
+            |> Array.ofList
+        let! results = Task.WhenAll(tasks)
+
+        let extractError r =
+            match r with
+            | Error e -> [e]
+            | _ -> []
+
+        let errors =
+            results 
+            |> List.ofArray
+            |> List.collect extractError
+
+        //for s in subscriptions do
+        //    let! getFeedResult = adapter.getFromUrl(s.Url)
+        //    match getFeedResult with
+        //    | Ok feed ->
+        //        for item in feed.Items do
+        //            let article = { Title = item.Title; Id = Guid.NewGuid() }
+        //            articleRepository.save(article)
+        //    | Error e -> ()
+
+        return Ok errors
     }
---
+++ Farss.Tests/FetchArticlesWorkflowTests.fs
@@ -0,0 +1,62 @@
+﻿module FetchArticlesWorkflowTests
+
+open Domain
+open Expecto
+open Persistence
+open FakeFeedReaderAdapter
+open System
+open FeedReaderAdapter
+
+let setup test = async {
+    
+        let subscriptionRepository = Persistence.create () 
+        let articleRepository = Persistence.ArticleRepositoryImpl.createInMemory()
+        let adapter = FakeFeedReaderAdapter.createStub ()
+
+        do! test subscriptionRepository articleRepository adapter
+    
+    }
+
+[<Tests>]
+let tests = 
+    testList "Fetch articles workflow" [
+        let tests = [
+            "Does nothing when no subscriptions exists", fun subs (articles: ArticleRepository) (adapterStub: FeedReaderAdapterStub) -> async {
+                let workflow = FetchEntriesWorkflow.fetchEntries subs articles adapterStub.Adapter
+                let! result = workflow () |> Async.AwaitTask
+                
+                Expect.isOk result "Workflow result"
+                Expect.isEmpty (articles.getAll()) "Articles"
+            }
+            "Does nothing when no items exists in feed", fun (subs: SubscriptionRepository) (articles: ArticleRepository) (adapterStub: FeedReaderAdapterStub) -> async {
+                subs.save ({ Url = "feed url"; Id = Guid.NewGuid() })
+                let emptyFeed = { FeedReaderAdapter.Feed.Title = ""; Description = ""; Items = [] }
+                adapterStub.SetResult ("feed url", Ok emptyFeed)
+
+                let workflow = FetchEntriesWorkflow.fetchEntries subs articles adapterStub.Adapter
+                let! result = workflow () |> Async.AwaitTask
+                
+                Expect.isOk result "Workflow result"
+                Expect.isEmpty (articles.getAll()) "Articles"
+            }
+            "Returns failure when feed fails", fun (subs: SubscriptionRepository) (articles: ArticleRepository) (adapterStub: FeedReaderAdapterStub) -> async {
+                subs.save ({ Url = "feed url"; Id = Guid.NewGuid() })
+                let expectedError = FeedError.ParseError (exn("error"))
+                adapterStub.SetResult ("feed url", Error expectedError)
+
+                let workflow = FetchEntriesWorkflow.fetchEntries subs articles adapterStub.Adapter
+                let! result = workflow () |> Async.AwaitTask
+                
+                Expect.equal result (Ok [ expectedError ]) "Expected error"
+                Expect.isEmpty (articles.getAll()) "Articles"
+            }
+        ]
+        yield! testFixtureAsync setup tests
+
+        //no feeds, no articles does nothing
+        //feed with no articles does nothing
+        //feed with one new article adds one
+        //feed with existing article does nothing
+        //multiple feeds multiple articles, read and not read
+        //failing reads?
+    ]
