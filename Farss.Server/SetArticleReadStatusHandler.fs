ffea6e4e8d5f5f87a5a00d421fda0eed4353f753
chris <chris.janssom@gmail.com>
2018-12-16 08:49:11 +0100

    Convert result type

--- Farss.Server/SetArticleReadStatusHandler.fs
+++ Farss.Server/SetArticleReadStatusHandler.fs
@@ -5,6 +5,16 @@ open Microsoft.AspNetCore.Http
 open FSharp.Control.Tasks.V2
 open Persistence
 open Dto
+open SetArticleReadStatusWorkflow
+
+let convertToHandler (result: Result<Unit, SetArticleReadStatusWorkflow.WorkflowError>) =
+    match result with
+    | Ok _ -> Successful.NO_CONTENT
+    | Error (InvalidParameter parameters) -> 
+        let message = sprintf "Invalid parameters: %s" <| System.String.Join(",", parameters)
+        RequestErrors.BAD_REQUEST message
+    | Error ArticleNotFound -> 
+        RequestErrors.BAD_REQUEST "Article not found"
 
 let setArticleReadStatusHandler: HttpHandler =
     fun (next: HttpFunc) (ctx: HttpContext) -> task {
@@ -12,8 +22,8 @@ let setArticleReadStatusHandler: HttpHandler =
         let ar = ctx.GetService<ArticleRepository>()
 
         let workflow = SetArticleReadStatusWorkflow.setArticleReadStatusWorkflowImpl ar
-        do workflow cmd |> ignore //todo: convert to response
-
-        return! Successful.NO_CONTENT next ctx
+        let handler = workflow >> convertToHandler
+        
+        return! handler cmd next ctx
     }
 
