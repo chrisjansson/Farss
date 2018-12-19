71589d0de4d1214b7ba07e8bf036a7561e1a56e9
chris <chris.janssom@gmail.com>
2018-12-19 19:31:20 +0100

    Load articles and subscriptions on startup

--- Farss.Client/src/App.fs
+++ Farss.Client/src/App.fs
@@ -1,43 +1,74 @@
 module App
 
-(**
- The famous Increment/Decrement ported from Elm.
- You can find more info about Emish architecture and samples at https://elmish.github.io/
-*)
-
 open Elmish
 open Elmish.React
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
-// MODEL
+type Model =
+    | Loading
+    | Loaded of exn * exn
 
-type Model = int
+type Msg = 
+    | Loaded of Dto.SubscriptionDto list * Dto.ArticleDto list
+    | LoadingError of string
 
-type Msg =
-| Increment
-| Decrement
+module PromiseResult =
+    let map f p = promise {
+        let! res = p
+        return 
+            match res with
+            | Ok v -> Ok (f v)
+            | Error e -> Error e
+    }
 
-let init() : Model = 0
+    let bind f p = promise {
+        let! res = p
+        match res with
+        | Ok v -> return! f v
+        | Error e -> return Error e
+    }    
 
-// UPDATE
+module Cmd =
+    let ofPromiseResult 
+        (task: 'arg -> Fable.Import.JS.Promise<Result<'a, 'e>>) 
+        (arg: 'arg) (onSuccess: 'a -> 'msg) 
+        (onError: 'e -> 'msg)
+        : Cmd<'msg>  =
+        let executeTask task arg dispatch = promise {
+                let! result = task arg
+                match result with
+                | Ok r -> 
+                    let m = onSuccess r
+                    dispatch m
+                | Error e ->
+                    let m = onError e
+                    dispatch m
+            }
+            
+        let bind: Sub<'msg> = 
+            fun dispatch ->
+                executeTask task arg dispatch |> ignore
+        [bind]
+
+let init(): Model * Cmd<Msg> = 
+    let loadSubsAndArticles () = 
+        ApiClient.getSubscriptions ()
+        |> PromiseResult.bind(fun r -> ApiClient.getArticles () |> PromiseResult.map (fun r2 -> r, r2))
+
+    let cmd = Cmd.ofPromiseResult loadSubsAndArticles () Msg.Loaded Msg.LoadingError
+    Loading, cmd
 
 let update (msg:Msg) (model:Model) =
-    match msg with
-    | Increment -> model + 1
-    | Decrement -> model - 1
-
-// VIEW (rendered with React)
+    model, Cmd.none
 
 let view (model:Model) dispatch =
-
   div []
-      [ button [ OnClick (fun _ -> dispatch Increment) ] [ str "+" ]
+      [ button [  ] [ str "+" ]
         div [] [ str (string model) ]
-        button [ OnClick (fun _ -> dispatch Decrement) ] [ str "-" ] ]
+        button [ ] [ str "-" ] ]
 
-// App
-Program.mkSimple init update view
-|> Program.withReact "elmish-app"
-|> Program.withConsoleTrace
-|> Program.run
+Program.mkProgram init update view
+    |> Program.withReact "elmish-app"
+    |> Program.withConsoleTrace
+    |> Program.run
