fa339d30f1a8c4fd736b673f12fb31b1fde7d426
chris <chris.janssom@gmail.com>
2018-12-21 07:46:02 +0100

    Checking if it's feasable to get rid of dispatch passing
    Like in elm

--- Farss.Client/src/App.fs
+++ Farss.Client/src/App.fs
@@ -72,6 +72,38 @@ let renderLoading () =
 let onClick dispatch msg =
     OnClick (fun _ -> dispatch msg)
 
+module HTML =
+    type Attr<'msg> = 
+        | OnClick of 'msg
+        | Type of string
+        | Value of string
+
+    let onClick msg = OnClick msg
+
+    type Html<'msg> = Dispatch<'msg> -> Fable.Import.React.ReactElement
+
+    module R = Fable.Helpers.React
+    module Props = Fable.Helpers.React.Props
+
+    let convertToProp attr dispatch =
+        match attr with
+        | OnClick msg -> Props.OnClick (fun _ -> dispatch msg) :> IHTMLProp
+        | Type t -> Props.Type t :> IHTMLProp
+        | Value v -> Props.Value v :> IHTMLProp
+
+    let convertToProps props dispatch = Seq.map (fun p -> convertToProp p dispatch) props
+
+    let applyDispatch (elements: Html<'msg> seq) (dispatch: Dispatch<'msg>) = 
+        Seq.map (fun e -> e dispatch) elements
+
+    let input (props: Attr<'msg> seq): Html<'msg> =
+        fun d ->
+            let props = convertToProps props d
+            input props
+
+    let fragment () (elements: Html<'msg> seq): Html<'msg> =
+        fun d -> R.fragment [] (applyDispatch elements d)
+
 let renderLoaded (model: (Dto.SubscriptionDto list * Dto.ArticleDto list)) dispatch =
     let subscriptions, articles = model
     
