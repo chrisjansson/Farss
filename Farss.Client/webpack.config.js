120c9f82c0c2d8228c61c329df1ff977c98e0e58
chris <chris.janssom@gmail.com>
2018-12-22 10:45:58 +0100

    Service to all networks

--- Farss.Client/webpack.config.js
+++ Farss.Client/webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
     },
     devServer: {
         contentBase: "./public",
+        host: '0.0.0.0',
         port: 8080,
         proxy: {
             "/api": "http://localhost:5000"
