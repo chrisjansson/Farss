b11eed652e23a1a6517f4bbd8426e9558ba3d79a
chris <chris.janssom@gmail.com>
2019-01-14 08:01:41 +0100

    Create/Validate ArticleGuids

--- Farss.Server/Domain.fs
+++ Farss.Server/Domain.fs
@@ -13,13 +13,21 @@ type Article =
     {
         Id: ArticleId
         Title: string
-        Guid: string
+        Guid: ArticleGuid
         Subscription: SubscriptionId
         Content: string
         IsRead: bool
         PublishedAt: DateTimeOffset
     }
 and ArticleId = Guid
+and ArticleGuid = string //TODO: wrap in DU?
+
+module ArticleGuid =
+    let create (str: string) = 
+        if String.IsNullOrWhiteSpace(str) then
+            Error "Article guid cannot be null or empty"
+        else
+            Ok str 
 
 type DeleteSubscriptionCommand = { Id: SubscriptionId }
 type SetArticleReadStatusCommand = { ArticleId: ArticleId; SetIsReadTo: bool }
---
+++ Farss.Tests/CreateArticleTests.fs
@@ -0,0 +1,34 @@
+﻿module CreateArticleTests
+
+open Expecto
+open Domain
+
+[<Tests>]
+let tests = testList "Create article tests" [
+        testList "Article Guid" [
+            let invalidCases = [
+                ("Cannot be null", null)
+                ("Cannot be empty string", "")
+                ("Cannot be only whitespace", "   ")
+                ("Cannot be only whitespace tabs", "\t")
+            ]
+
+            let testInvalidArticleGuid (name, value) =
+                testCase name <| fun _ ->
+                    let result = ArticleGuid.create value
+                    Expect.isError result "Should be invalid null or empty or whitespace"
+
+            yield! List.map testInvalidArticleGuid invalidCases
+
+            let validCases = [
+                ("A valid example", "a globally unique identifier in string form")
+            ]
+
+            let testValidArticleGuid (name, value) =
+                testCase name <| fun _ ->
+                    let result = ArticleGuid.create value
+                    Expect.isOk result "Should be a valid article guid"
+
+            yield! List.map testValidArticleGuid validCases
+        ]
+    ]
