860f6abab9cedcbb1bdc1f715dfc66a4233b26c1
chris <chris.janssom@gmail.com>
2019-01-15 22:05:56 +0100

    Create article from feed item

---
+++ Farss.Tests/CreateArticleFromFeedItemTests.fs
@@ -0,0 +1,53 @@
+﻿module CreateArticleFromFeedItemTests
+
+open Expecto
+open System
+
+module FeedItem = 
+    open Domain
+
+    let toArticle subscriptionId (item: FeedReaderAdapter.Item) = result {
+        let! guid = ArticleGuid.create item.Id
+        let! timestamp = ArticleTimestamp.create item.Timestamp
+
+        return Article.create item.Title guid subscriptionId item.Content timestamp
+    }
+
+[<Tests>]
+let tests = 
+    testList "Create article from feed item" [
+        let subscriptionId = Guid.NewGuid()
+        let feedItemThatConvertsToArticle: FeedReaderAdapter.Item =
+            {
+                Title = "title"
+                Id = "guid"
+                Content = "content"
+                Timestamp = Some (DateTimeOffset(2001, 2, 3, 4, 5, 6, TimeSpan.Zero))
+            }
+
+        yield testCase "Item id is required" <| fun _ ->
+            let item = { feedItemThatConvertsToArticle with Id = null }
+
+            let result = FeedItem.toArticle subscriptionId item
+            
+            Expect.isError result "Item Id is required"
+
+        yield testCase "Timestamp is required" <| fun _ ->
+            let item = { feedItemThatConvertsToArticle with Timestamp = None }
+
+            let result = FeedItem.toArticle subscriptionId item
+            
+            Expect.isError result "Item timestamp is required"
+            
+        yield testCase "Creates article when all requirements are met" <| fun _ ->
+            let result = FeedItem.toArticle subscriptionId feedItemThatConvertsToArticle
+            
+            Expect.isOk result "Creates article from item successfully"
+            
+            let article: Domain.Article = result |> fun (Ok(article)) -> article
+
+            Expect.equal article.Guid feedItemThatConvertsToArticle.Id "Guid"
+            Expect.equal article.Content feedItemThatConvertsToArticle.Content "Content"
+            Expect.equal article.PublishedAt feedItemThatConvertsToArticle.Timestamp.Value "Timestamp"
+            Expect.equal article.Title feedItemThatConvertsToArticle.Title "Title"
+    ]
