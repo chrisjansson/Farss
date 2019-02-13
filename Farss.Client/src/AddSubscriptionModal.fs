aa81d66dc3bac72c24ad19ec6f1087846eafe3e9
chris <chris.janssom@gmail.com>
2019-02-13 13:24:21 +0100

    Bulma modal scaffolding

--- Farss.Client/src/AddSubscriptionModal.fs
+++ Farss.Client/src/AddSubscriptionModal.fs
@@ -1,6 +1,9 @@
 ﻿module AddSubscriptionModal
 
 open Elmish
+open ModalPortal
+
+module R =  Fable.Helpers.React
 //Open
 //Paste url of feed/website
 //Ok/Next (poll to check if feed works)
@@ -35,3 +38,81 @@ let udpate (msg: Message) (model: Model) =
         Model.PreviewSubscription, Cmd.none
     | (LoadingPreview, SubscriptionPreviewReceived (Error e)) ->
         PreviewFeedFailed, Cmd.none
+
+module TextResources =
+    [<Literal>]
+    let AddSubscriptionModalTitle = "Add subscription"
+
+    [<Literal>]
+    let OkButtonTitle = "Ok"
+
+    [<Literal>]
+    let CancelButtonTitle = "Cancel"
+
+module CardModal =
+
+    open Fulma
+    open Fable.Helpers.React
+    
+    type Settings<'msg> = 
+        {
+            Title: string
+            Buttons: ButtonModel<'msg> list
+        }
+
+    and ButtonModel<'msg> = 
+        {
+            OnClick: unit -> 'msg 
+            Title: string
+            Options: Button.Option list
+        }
+
+    let defaultSettings () = 
+        {
+            Title = "Default text"
+            Buttons = []
+        }
+
+    let cardModal (settings: Settings<_>) closeDisplay dispatch =
+        let renderButton (model: ButtonModel<'msg>) =
+            let onClick = Button.Option.OnClick (fun _ -> dispatch (model.OnClick ()))
+            Button.button (onClick::model.Options) [ str model.Title ]
+        let renderFooterButtons (buttons: ButtonModel<'msg> list) =
+            buttons |> List.map renderButton
+        Modal.modal [ Modal.IsActive true ]
+            [ Modal.background [ Props [ Props.OnClick closeDisplay ] ] [ ]
+              Modal.Card.card [ ]
+                [ Modal.Card.head [ ]
+                    [ Modal.Card.title [ ]
+                        [ str settings.Title ]
+                      Delete.delete [ Delete.OnClick closeDisplay ] [ ] ]
+                  Modal.Card.body [ ]
+                    [ str "Some content" ]
+                  Modal.Card.foot [ ] (renderFooterButtons settings.Buttons) ] ]
+
+let view model = 
+    let modalSettings = 
+        { CardModal.defaultSettings () with 
+            Title = TextResources.AddSubscriptionModalTitle 
+            Buttons = [
+                { 
+                    Title = TextResources.OkButtonTitle; 
+                    OnClick = fun _ -> (); 
+                    Options = [ Fulma.Button.Color Fulma.Color.IsSuccess ] 
+                }
+                {
+                    Title = TextResources.CancelButtonTitle; 
+                    OnClick = fun _ -> (); 
+                    Options = [] 
+                }
+            ]
+        }
+    
+    modalPortal [
+
+
+        CardModal.cardModal modalSettings ignore ignore
+        //R.button [] [ R.str "Hello!!" ]
+    ]
+
+
