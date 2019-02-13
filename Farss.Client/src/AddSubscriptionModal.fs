e34146489959adc93f16e3df34773198a5bee934
chris <chris.janssom@gmail.com>
2019-02-13 13:41:15 +0100

    Extractm odal

--- Farss.Client/src/AddSubscriptionModal.fs
+++ Farss.Client/src/AddSubscriptionModal.fs
@@ -49,57 +49,10 @@ module TextResources =
     [<Literal>]
     let CancelButtonTitle = "Cancel"
 
-module CardModal =
-
-    open Fulma
-    open Fable.Helpers.React
-    
-    type Settings<'msg> = 
-        {
-            Title: string
-            Buttons: ButtonModel<'msg> list
-            Close: (unit -> 'msg) option
-        }
-
-    and ButtonModel<'msg> = 
-        {
-            OnClick: unit -> 'msg 
-            Title: string
-            Options: Button.Option list
-        }
-
-    let defaultSettings () = 
-        {
-            Title = "Default text"
-            Buttons = []
-            Close = None
-        }
-
-    let cardModal (settings: Settings<_>) children dispatch =
-        let renderButton (model: ButtonModel<'msg>) =
-            let onClick = Button.Option.OnClick (fun _ -> dispatch (model.OnClick ()))
-            Button.button (onClick::model.Options) [ str model.Title ]
-
-        let renderFooterButtons (buttons: ButtonModel<'msg> list) =
-            buttons |> List.map renderButton
-
-        Modal.modal [ Modal.IsActive true ] [ 
-            Modal.background [ ] [ ]
-            Modal.Card.card [ ] [ 
-                Modal.Card.head [ ] [ 
-                    yield Modal.Card.title [ ] [ str settings.Title ]
-                    match settings.Close with
-                    | Some m -> yield Delete.delete [ Delete.OnClick (fun _ -> dispatch (m ())) ] [ ] 
-                    | None -> yield! []
-                ]
-                Modal.Card.body [ ] children
-                Modal.Card.foot [ ] (renderFooterButtons settings.Buttons)
-            ]
-        ]
 
 let view model = 
     let modalSettings = 
-        { CardModal.defaultSettings () with 
+        { Modal.defaultSettings () with 
             Title = TextResources.AddSubscriptionModalTitle 
             Buttons = [
                 { 
@@ -116,7 +69,7 @@ let view model =
         }
     
     modalPortal [
-        CardModal.cardModal modalSettings [ Fable.Helpers.React.str "Some content" ] ignore
+        Modal.cardModal modalSettings [ Fable.Helpers.React.str "Some content" ] ignore
     ]
 
 
---
+++ Farss.Client/src/Modal.fs
@@ -0,0 +1,47 @@
+﻿module Modal
+
+open Fulma
+open Fable.Helpers.React
+    
+type Settings<'msg> = 
+    {
+        Title: string
+        Buttons: ButtonModel<'msg> list
+        Close: (unit -> 'msg) option
+    }
+
+and ButtonModel<'msg> = 
+    {
+        OnClick: unit -> 'msg 
+        Title: string
+        Options: Button.Option list
+    }
+
+let defaultSettings () = 
+    {
+        Title = "Default text"
+        Buttons = []
+        Close = None
+    }
+
+let cardModal (settings: Settings<_>) children dispatch =
+    let renderButton (model: ButtonModel<'msg>) =
+        let onClick = Button.Option.OnClick (fun _ -> dispatch (model.OnClick ()))
+        Button.button (onClick::model.Options) [ str model.Title ]
+
+    let renderFooterButtons (buttons: ButtonModel<'msg> list) =
+        buttons |> List.map renderButton
+
+    Modal.modal [ Modal.IsActive true ] [ 
+        Modal.background [ ] [ ]
+        Modal.Card.card [ ] [ 
+            Modal.Card.head [ ] [ 
+                yield Modal.Card.title [ ] [ str settings.Title ]
+                match settings.Close with
+                | Some m -> yield Delete.delete [ Delete.OnClick (fun _ -> dispatch (m ())) ] [ ] 
+                | None -> yield! []
+            ]
+            Modal.Card.body [ ] children
+            Modal.Card.foot [ ] (renderFooterButtons settings.Buttons)
+        ]
+    ]
