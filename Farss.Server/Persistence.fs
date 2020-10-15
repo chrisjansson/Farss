e93b7a5b6a48e3a6a953cb9fceb9c5702f907565
Chris Jansson <chris.janssom@gmail.com>
2020-10-15 10:56:44 +0200

    Store subscriptions with EF instead

--- Farss.Server/Persistence.fs
+++ Farss.Server/Persistence.fs
@@ -38,10 +38,12 @@ type WorkflowError =
     | BadRequest of string * System.Exception
     | InvalidParameter of string list
 
+open System.Linq.Expressions
+open System.Linq
+open System
+
 module Query = 
-    open System.Linq.Expressions
-    open System.Linq
-    open System
+
 
     let where (predicate: Expression<Func<_, bool>>) (query: IQueryable<_>) =
         query.Where(predicate)
@@ -58,23 +60,58 @@ module Query =
     type Expr = 
         static member Quote(e:Expression<System.Func<_, _>>) = e
 
-module SubscriptionRepositoryImpl =
-    open Marten
+[<AllowNullLiteral>]
+type PersistedSubscription() =
+    member val Id: Guid = Unchecked.defaultof<_> with get, set
+    member val Url: string = Unchecked.defaultof<_> with get, set
+    member val Title: string = Unchecked.defaultof<_> with get, set
+
+open Microsoft.EntityFrameworkCore
+
+type ReaderContext(options) =
+    inherit DbContext(options)
     
-    let create (documentSession: IDocumentSession) =
+    member val Subscriptions: DbSet<PersistedSubscription> = null with get, set
+
+module SubscriptionRepositoryImpl =
+    let E = Query.Expr.Quote
+    
+    let private mapToSubscription (s: PersistedSubscription): Subscription =
+        {
+            Id = s.Id
+            Url = s.Url
+            Title = s.Title
+        }
+    
+    let private mapFromSubscription (s: Subscription) (t: PersistedSubscription) =
+        t.Id <- s.Id
+        t.Title <- s.Title
+        t.Url <- t.Url
+    
+    
+    let create (context: ReaderContext) =
         let getAll () = 
-            documentSession.Query<Subscription>()
+            context.Subscriptions
             |> Query.toList
+            |> Seq.map mapToSubscription
             |> List.ofSeq
         let save (subscription: Subscription) =
-            documentSession.Store(subscription)
-            documentSession.SaveChanges()
+            context.Subscriptions.Find(subscription.Id)
+            |> Option.ofObj
+            |> Option.defaultWith (fun () ->
+                let i = PersistedSubscription()
+                context.Subscriptions.Add(i) |> ignore
+                i)
+            |> mapFromSubscription subscription
+            context.SaveChanges() |> ignore
         let delete (subscriptionId: SubscriptionId) =
-            documentSession.Delete<Subscription>(subscriptionId)
-            documentSession.SaveChanges()
-        let get (subscriptionId: SubscriptionId) =  
-            documentSession.Query<Subscription>()
-                |> Query.singleP (Query.Expr.Quote(fun s -> s.Id = subscriptionId))
+            let s = context.Subscriptions.Find(subscriptionId)
+            context.Subscriptions.Remove(s) |> ignore
+            context.SaveChanges() |> ignore
+        let get (subscriptionId: SubscriptionId) =
+            context.Subscriptions
+                .Single(fun s -> s.Id = subscriptionId)
+                |> mapToSubscription
         {
             get = get
             getAll = getAll
