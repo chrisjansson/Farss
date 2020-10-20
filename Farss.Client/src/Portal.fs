eccae393e4ce39a4d5db994668c7b6eb545236e3
Chris Jansson <chris.janssom@gmail.com>
2020-10-20 09:16:23 +0200

    Refactor portal to feliz

---
+++ Farss.Client/src/Portal.fs
@@ -0,0 +1,23 @@
+﻿module ModalPortal
+
+open Feliz
+open Fable.React
+
+let private portalRoot = Browser.Dom.document.getElementById(ReactSettings.modalRootId)
+
+type private Portal() = 
+    inherit Component<unit, unit>()
+
+    let element = Browser.Dom.document.createElement("div")
+
+    override __.componentDidMount() = 
+        portalRoot.appendChild(element) |> ignore
+
+    override __.componentWillUnmount() =
+        portalRoot.removeChild(element) |> ignore
+
+    override this.render() = 
+        ReactDOM.createPortal(React.fragment this.children ,element)
+
+let portal (children: _ seq) =
+    Helpers.ofType<Portal, _, _> () children
