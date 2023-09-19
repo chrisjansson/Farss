f066aa31b4bad86574e99286fe89e229b3b3cda8
Chris Jansson <chris.jansson@hups.com>
2023-09-19 15:31:45 +0200

    Untrack files

--- Farss.Shared/ApiUrls.fs.js
+++
@@ -1,8 +0,0 @@
-import { printf, toConsole } from "../Farss.Client/src/fable_modules/fable-library.3.7.18/String.js";
-
-export const GetFileRoute = "/api/file/{id}";
-
-export function GetFile(id) {
-    toConsole(printf("/api/file/%A"))(id);
-}
-
