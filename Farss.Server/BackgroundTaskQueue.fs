8f968fc7894ef0b06ef20371419ec3e22b0ac0fb
Chris Jansson <chris.jansson@hups.com>
2023-09-25 10:20:26 +0200

    Add another job type queue

--- Farss.Server/BackgroundTaskQueue.fs
+++ Farss.Server/BackgroundTaskQueue.fs
@@ -3,6 +3,7 @@
 open System.Threading
 open System.Threading.Channels
 open System.Threading.Tasks
+open Domain
 
 
 type QueueReason =
@@ -12,9 +13,15 @@ type QueueReason =
 type IBackgroundTaskQueue =
     abstract member QueuePollArticles: QueueReason -> ValueTask
     abstract member DequeuePollArticles: CancellationToken -> ValueTask<QueueReason>
+    abstract member DequeuePollArticlesForSubscription: CancellationToken -> ValueTask<SubscriptionId>
+    abstract member QueuePollArticlesForSubscription: SubscriptionId * CancellationToken -> ValueTask
 
 type BackgroundTaskQueue() =
     let queue: Channel<_> =
+        let options = BoundedChannelOptions(100)
+        Channel.CreateBounded<_>(options)
+        
+    let pollArticlesQueue: Channel<_> =
         let options = BoundedChannelOptions(100)
         Channel.CreateBounded<_>(options)
         
@@ -23,4 +30,11 @@ type BackgroundTaskQueue() =
             queue.Writer.WriteAsync((fetchReason))
         member this.DequeuePollArticles(ct) =
             queue.Reader.ReadAsync(ct)
+
+        member this.DequeuePollArticlesForSubscription(ct) =
+            pollArticlesQueue.Reader.ReadAsync(ct)
+            
+        member this.QueuePollArticlesForSubscription(subscriptionId, ct) =
+            pollArticlesQueue.Writer.WriteAsync(subscriptionId, ct)
+        
         
