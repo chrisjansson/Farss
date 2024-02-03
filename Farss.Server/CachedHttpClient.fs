cd153095ab64d4c59992b88aa92cba341ee47f18
Chris Jansson <chris@chrisjansson.se>
2024-02-03 09:39:16 +0100

    Fetch content with optional ETag and Modified-Since headers

--- Farss.Server/CachedHttpClient.fs
+++ Farss.Server/CachedHttpClient.fs
@@ -3,83 +3,86 @@
 open System
 open System.Net
 open System.Net.Http
-open System.Net.Http.Headers
 
-    //
-    // public static async Task<byte[]> DownloadBytesAsync(
-    //   string url,
-    //   CancellationToken cancellationToken,
-    //   bool autoRedirect = true,
-    //   string userAgent = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0")
-    // {
-    //   url = WebUtility.UrlDecode(url);
-    //   HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
-    //   HttpResponseMessage httpResponseMessage;
-    //   try
-    //   {
-    //     request.Headers.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
-    //     request.Headers.TryAddWithoutValidation("User-Agent", userAgent);
-    //     httpResponseMessage = await Helpers._httpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead, cancellationToken).ConfigureAwait(false);
-    //   }
-    //   finally
-    //   {
-    //     request?.Dispose();
-    //   }
-    //   request = (HttpRequestMessage) null;
-    //   if (!httpResponseMessage.IsSuccessStatusCode)
-    //   {
-    //     int statusCode = (int) httpResponseMessage.StatusCode;
-    //     if (autoRedirect && (statusCode == 301 || statusCode == 302 || statusCode == 308))
-    //       url = httpResponseMessage.Headers?.Location?.AbsoluteUri ?? url;
-    //     request = new HttpRequestMessage(HttpMethod.Get, url);
-    //     try
-    //     {
-    //       httpResponseMessage = await Helpers._httpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead, cancellationToken).ConfigureAwait(false);
-    //     }
-    //     finally
-    //     {
-    //       request?.Dispose();
-    //     }
-    //     request = (HttpRequestMessage) null;
-    //   }
-    //   return await httpResponseMessage.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
-    // }
+//
+// public static async Task<byte[]> DownloadBytesAsync(
+//   string url,
+//   CancellationToken cancellationToken,
+//   bool autoRedirect = true,
+//   string userAgent = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0")
+// {
+//   url = WebUtility.UrlDecode(url);
+//   HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, url);
+//   HttpResponseMessage httpResponseMessage;
+//   try
+//   {
+//     request.Headers.TryAddWithoutValidation("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
+//     request.Headers.TryAddWithoutValidation("User-Agent", userAgent);
+//     httpResponseMessage = await Helpers._httpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead, cancellationToken).ConfigureAwait(false);
+//   }
+//   finally
+//   {
+//     request?.Dispose();
+//   }
+//   request = (HttpRequestMessage) null;
+//   if (!httpResponseMessage.IsSuccessStatusCode)
+//   {
+//     int statusCode = (int) httpResponseMessage.StatusCode;
+//     if (autoRedirect && (statusCode == 301 || statusCode == 302 || statusCode == 308))
+//       url = httpResponseMessage.Headers?.Location?.AbsoluteUri ?? url;
+//     request = new HttpRequestMessage(HttpMethod.Get, url);
+//     try
+//     {
+//       httpResponseMessage = await Helpers._httpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead, cancellationToken).ConfigureAwait(false);
+//     }
+//     finally
+//     {
+//       request?.Dispose();
+//     }
+//     request = (HttpRequestMessage) null;
+//   }
+//   return await httpResponseMessage.Content.ReadAsByteArrayAsync().ConfigureAwait(false);
+// }
 
+type Response =
+    | Ok of {| Content: string; ETag: string option; LastModified: DateTimeOffset option |}
+    | NotModified
+    | Error
 
-
-let doWork (url: string, etag: string option, lastModified: DateTimeOffset option) =
-    
+//Doesnt follow redirects, no timeouts nor cancellation tokens, error handling
+let get (url: string, etag: string option, lastModified: DateTimeOffset option) =
     task {
-        use httpClient = new HttpClient()    
-    
+        use httpClient = new HttpClient()
+
         let request =
             let userAgent = "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0"
             let url = WebUtility.UrlDecode(url)
-        
+
             let request = new HttpRequestMessage(HttpMethod.Get, url)
-        
+
             match etag with
             | Some etag -> request.Headers.IfNoneMatch.ParseAdd(etag)
             | None -> ()
-            
+
             match lastModified with
             | Some lastModified -> request.Headers.Add("If-Modified-Since", lastModified.ToString("R"))
             | None -> ()
-            
+
             request.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
             request.Headers.Add("User-Agent", userAgent)
-            
+
             request
-        
+
         let! response = httpClient.SendAsync(request)
-        
-        let etag =
-            response.Headers.ETag
-            |> Option.ofObj
-            
-        let lastModified =
-            response.Content.Headers.LastModified
-            |> Option.ofNullable
-        
-        return etag, lastModified
+
+        match response.StatusCode with
+        | HttpStatusCode.OK ->
+            let etag = response.Headers.ETag.Tag |> Option.ofObj
+            let lastModified = response.Content.Headers.LastModified |> Option.ofNullable
+            let! content = response.Content.ReadAsStringAsync()
+            return Ok {| Content = content; ETag = etag; LastModified = lastModified |}
+        | HttpStatusCode.NotModified ->
+            return NotModified
+        | _ ->
+            return Error
     }
--- Farss.Tests/HttpTests.fs
+++ Farss.Tests/HttpTests.fs
@@ -12,8 +12,8 @@ let tests =
             //What does 304 return in body?
 
             // let! result = CachedHttpClient.doWork "https://www.fz.se/"
-            let! result = CachedHttpClient.doWork ("https://blog.ploeh.dk/atom.xml", None, DateTimeOffset.Parse("Fri, 02 Feb 2024 12:54:55 GMT") |> Some)
-
+            // let! result = CachedHttpClient.doWork ("https://blog.ploeh.dk/atom.xml", None, DateTimeOffset.Parse("Fri, 02 Feb 2024 12:54:55 GMT") |> Some)
+            let! result = CachedHttpClient.get ("https://blog.ploeh.dk/atom.xml", Some "\"65bce61f-10bb139\"", None)
 
             //Usage: Lookup previous ETag and Last-modified, check result for content. If 304 etc get previous response or shortcut parse
             
