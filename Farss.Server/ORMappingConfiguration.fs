be4341af11464883102930ca8d72248586a0fb71
Chris Jansson <chris@chrisjansson.se>
2024-06-08 12:14:10 +0200

    Set tenantId

--- Farss.Server/ORMappingConfiguration.fs
+++ Farss.Server/ORMappingConfiguration.fs
@@ -1,12 +1,18 @@
 module ORMappingConfiguration
 
 open System
+open System.Security.Claims
 open Entities
 open System.Collections.Generic
+open Microsoft.AspNetCore.Http
 open Microsoft.EntityFrameworkCore
 
-type ReaderContext(options) =
+type ReaderContext(options, httpContextAccessor: IHttpContextAccessor) =
     inherit DbContext(options)
+    let tenantId =
+        let user = httpContextAccessor.HttpContext.User
+        let claim = user.FindFirst(ClaimTypes.NameIdentifier)
+        Guid.Parse(claim.Value)
 
     [<DefaultValue>]
     val mutable subscriptions: DbSet<PersistedSubscription>
@@ -43,7 +49,7 @@ type ReaderContext(options) =
     member x.SubscriptionLogEntries
         with get () = x.persistedSubscriptionLogEntries
         and set v = x.persistedSubscriptionLogEntries <- v
-        
+
     [<DefaultValue>]
     val mutable persistedUsers: DbSet<PersistedUser>
 
@@ -52,66 +58,47 @@ type ReaderContext(options) =
         and set v = x.persistedUsers <- v
 
     override x.OnModelCreating(mb) =
-
-        let tenantId = Guid.Parse "D6B09244-40DA-4454-A214-E4F13798E245"
-        
         mb
             .Entity<PersistedArticle>()
             .HasOne(fun x -> x.Subscription)
             .WithMany(fun x -> x.Articles :> IEnumerable<_>)
         |> ignore
 
-        mb
-            .Entity<PersistedArticle>()
-            .Property<Guid>("TenantId")
-        |> ignore
-        
+        mb.Entity<PersistedArticle>().Property<Guid>("TenantId") |> ignore
+
         mb
             .Entity<PersistedArticle>()
             .HasQueryFilter(fun a -> EF.Property<Guid>(a, "TenantId") = tenantId)
         |> ignore
-        
+
         mb
             .Entity<PersistedSubscriptionLogEntry>()
             .HasOne(fun x -> x.Subscription)
             .WithMany()
         |> ignore
-                
+
         mb
             .Entity<PersistedSubscriptionLogEntry>()
             .HasQueryFilter(fun e -> EF.Property<Guid>(e, "TenantId") = tenantId)
         |> ignore
-        
-        mb
-            .Entity<PersistedSubscriptionLogEntry>()
-            .Property<Guid>("TenantId")
-        |> ignore
-        
+
+        mb.Entity<PersistedSubscriptionLogEntry>().Property<Guid>("TenantId") |> ignore
+
         mb
             .Entity<PersistedSubscription>()
             .HasOne<PersistedFile>()
             .WithMany()
             .HasForeignKey(nameof Unchecked.defaultof<PersistedSubscription>.IconId)
         |> ignore
-        
-        mb
-            .Entity<PersistedSubscription>()
-            .Property<Guid>("TenantId")
-        |> ignore
-        
+
+        mb.Entity<PersistedSubscription>().Property<Guid>("TenantId") |> ignore
+
         mb
             .Entity<PersistedSubscription>()
             .HasQueryFilter(fun e -> EF.Property<Guid>(e, "TenantId") = tenantId)
         |> ignore
-        
-        mb
-            .Entity<PersistedUser>()
-            .Property(fun e -> e.Username)
-            .IsRequired()
-        |> ignore
-        
-        mb
-            .Entity<PersistedUser>()
-            .HasIndex(fun e -> e.Username :> obj)
-            .IsUnique()
+
+        mb.Entity<PersistedUser>().Property(fun e -> e.Username).IsRequired() |> ignore
+
+        mb.Entity<PersistedUser>().HasIndex(fun e -> e.Username :> obj).IsUnique()
         |> ignore
